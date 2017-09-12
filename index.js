e9c58f0771d3964d95bfdea89b39a55cf87b2517
Anthony Achonye <tonexify@gmail.com>
2017-09-12 09:44:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return kittens.splice(0, kittens.length - 1)
+  return kittens.slice(0, kittens.length - 1)
 }
 
 function removeFirstKitten() {
-  return kittens.splice(1)
+  return kittens.slice(1)
 }
